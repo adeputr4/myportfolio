29478e6fccbd77d54154e76323a83ff3fed38eaf
adeputr4 <adedwiputra.id@gmail.com>
2019-05-22 01:19:33 +0700

    Updates

---
+++ precache-manifest.ec1836780b07c79cbf1d9c4d603698d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c464f8231007ec50c00bf91061c1774",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "140c3f49433ff29725ad",
+    "url": "/myportfolio/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "a19f38c573aa0dfbbfc4",
+    "url": "/myportfolio/static/css/main.ca6043fe.chunk.css"
+  },
+  {
+    "revision": "140c3f49433ff29725ad",
+    "url": "/myportfolio/static/js/2.b00df40b.chunk.js"
+  },
+  {
+    "revision": "a19f38c573aa0dfbbfc4",
+    "url": "/myportfolio/static/js/main.b646275a.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/myportfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a655ba64f13df138ceb90767d6f75d88",
+    "url": "/myportfolio/static/media/rocket.a655ba64.png"
+  }
+]);
